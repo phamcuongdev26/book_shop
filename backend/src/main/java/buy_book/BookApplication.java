7f3db3798db4c8655cc15c3dc2fbc246293f2fc1
shoppe <cuonzpham@gmail.com>
2026-05-16 16:30:27 +0700

    fix: remove EnableConfigurationProperties from BookApplication

--- backend/src/main/java/buy_book/BookApplication.java
+++ backend/src/main/java/buy_book/BookApplication.java
@@ -1,13 +1,9 @@
 package buy_book;
 
-import buy_book.properties.RsaKeyConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RsaKeyConfigProperties.class)
-
 public class BookApplication {
 
 	public static void main(String[] args) {
